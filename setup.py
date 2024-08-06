91c715a2d55744193b9a24f4682bb73a1b6d8fa9
sam-grant <samuel.grant94@gmail.com>
2024-08-06 16:08:20 +0000

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Mu2eEAF',
+    version='0.0.1',
+    author='Samuel Grant'
+    author_email='samuel.grant94@gmail.com'
+    description='Tools for Mu2e collaborators using EAF.', 
+    url='https://github.com/sam-grant/Mu2eEAF',
+    packages=['Mu2eEAF'],
+)
