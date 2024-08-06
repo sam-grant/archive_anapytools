e75d67ca24ccccc21a81df1599e69f5077268712
sam-grant <samuel.grant94@gmail.com>
2024-08-06 18:12:21 +0000

    Put packages back in setup.py.

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     author_email='samuel.grant94@gmail.com',
     description='Tools for Mu2e collaborators using EAF.', 
     url='https://github.com/sam-grant/Mu2eEAF',
-    # packages=['Mu2eEAF'],
+    packages=['Mu2eEAF'],
 )
