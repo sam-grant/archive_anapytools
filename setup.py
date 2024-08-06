033d2b7a60dd3c03ad88335da34b2449cbc8c86c
sam-grant <samuel.grant94@gmail.com>
2024-08-06 16:09:14 +0000

    Corrected syntax in setup.py.

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(
     name='Mu2eEAF',
     version='0.0.1',
-    author='Samuel Grant'
-    author_email='samuel.grant94@gmail.com'
+    author='Samuel Grant',
+    author_email='samuel.grant94@gmail.com',
     description='Tools for Mu2e collaborators using EAF.', 
     url='https://github.com/sam-grant/Mu2eEAF',
     packages=['Mu2eEAF'],
