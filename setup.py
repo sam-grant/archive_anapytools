6763b8b7250f2df90a38519b717254d3d8f97f3e
sam-grant <samuel.grant94@gmail.com>
2024-08-22 17:13:51 +0000

    Name changes.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='mu2e_tools',
+    name='mu2etools',
     version='0.0.1',
     author='Samuel Grant',
     author_email='samuel.grant94@gmail.com',
     description='Python tools for Mu2e collaborators using EAF.', 
-    url='https://github.com/sam-grant/mu2e_tools',
-    packages=['mu2e_tools'],
+    url='https://github.com/sam-grant/mu2etools',
+    packages=['mu2etools'],
 )
