6f70766a6c6becbd67751d48bf170116e348f474
sam-grant <samuel.grant94@gmail.com>
2024-08-22 16:58:40 +0000

    Refactoring.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='Mu2eEAF',
+    name='mu2e_tools',
     version='0.0.1',
     author='Samuel Grant',
     author_email='samuel.grant94@gmail.com',
-    description='Tools for Mu2e collaborators using EAF.', 
-    url='https://github.com/sam-grant/Mu2eEAF',
-    packages=['Mu2eEAF'],
+    description='Python tools for Mu2e collaborators using EAF.', 
+    url='https://github.com/sam-grant/mu2e_tools',
+    packages=['mu2e_tools'],
 )
